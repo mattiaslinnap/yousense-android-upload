a5b7ecdc883135c4d231676d59c09466b14022b0
Daniel Wagner <dtw30@cam.ac.uk>
2013-01-21 12:48:22 +0000

    added iso formatting

--- library/src/org/yousense/common/Time.java
+++ library/src/org/yousense/common/Time.java
@@ -1,9 +1,11 @@
 package org.yousense.common;
 
-import org.apache.commons.lang3.time.FastDateFormat;
-
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
+
+import org.apache.commons.lang3.time.FastDateFormat;
 
 public class Time {
 
@@ -11,6 +13,7 @@ public class Time {
     public static Locale US = Locale.US;
     public static FastDateFormat NOSPACES_MILLISECONDS = FastDateFormat.getInstance("yyyyMMdd_HHmmss_SSS", UTC, US);
     public static FastDateFormat TIMEONLY_MILLISECONDS = FastDateFormat.getInstance("HH:mm:ss.SSS", UTC, US);
+    public static FastDateFormat ISO_8604_WITH_RFC_822_TIME_ZONE = FastDateFormat.getInstance("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
 
     public static String timestampNoSpacesWithMilliseconds(long millis) {
         return NOSPACES_MILLISECONDS.format(millis);
@@ -26,5 +29,17 @@ public class Time {
 
     public static String timeOnlyWithMilliseconds() {
         return timeOnlyWithMilliseconds(System.currentTimeMillis());
+    }
+    
+    public static String isoFormat(Calendar cal) {
+    	return ISO_8604_WITH_RFC_822_TIME_ZONE.format(cal);
+    }
+    
+    public static String isoFormat(Date date) {
+    	return ISO_8604_WITH_RFC_822_TIME_ZONE.format(date);
+    }
+    
+    public static String isoFormat() {
+    	return isoFormat(new Date());
     }
 }
