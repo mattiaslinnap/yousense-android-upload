6ac43994f01459230770f0dcf8568b21fc0ebe7b
Daniel Wagner <dtw30@cam.ac.uk>
2013-01-25 16:41:57 +0000

    made compatible with - and changed minSdkVersion to - sdk level 8

--- library/src/org/yousense/eventlog/data/BuildData.java
+++ library/src/org/yousense/eventlog/data/BuildData.java
@@ -7,7 +7,6 @@ public class BuildData {
     public String model;
     public String device;
     public String display;
-    public String serial;
     public String os_release;
     public int os_sdk;
 
@@ -16,7 +15,6 @@ public class BuildData {
         this.model = Build.MODEL;
         this.device = Build.DEVICE;
         this.display = Build.DISPLAY;
-        this.serial = Build.SERIAL;
         this.os_release = Build.VERSION.RELEASE;
         this.os_sdk = Build.VERSION.SDK_INT;
     }
