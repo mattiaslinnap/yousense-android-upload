337be49285a9060bd54517b9d448574595cdc790
Daniel Wagner <dtw30@cam.ac.uk>
2013-02-14 16:25:32 +0000

    don't rely on encodeHexString in apache commons codec, which is unavailable in Android.

--- library/src/org/yousense/common/Hash.java
+++ library/src/org/yousense/common/Hash.java
@@ -1,12 +1,11 @@
 package org.yousense.common;
 
-import org.apache.commons.codec.binary.Hex;
-import org.apache.commons.codec.digest.DigestUtils;
-import org.apache.commons.io.FileUtils;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.io.FileUtils;
 
 /**
  * Helpers for Apache Commons Digest functions.
@@ -15,18 +14,27 @@ import java.io.IOException;
  * Apache Commons Codec library conflicts with the Android-shipped version, and somehow the sha1Hex function breaks.
  */
 public class Hash {
+	
+	private static String encodeHex(byte[] bytes) {
+		StringBuilder sb = new StringBuilder();
+	    for (byte b : bytes) {
+	        sb.append(String.format("%02X", b));
+	    }
+	    return sb.toString();
+	}
+	
     public static String sha1Hex(String data) {
-        return new String(Hex.encodeHex(DigestUtils.sha1(data))).toLowerCase();
+        return new String(encodeHex(DigestUtils.sha1(data))).toLowerCase();
     }
 
     public static String sha1Hex(String data, String salt) {
-        return new String(Hex.encodeHex(DigestUtils.sha1(salt + data + salt))).toLowerCase();
+        return new String(encodeHex(DigestUtils.sha1(salt + data + salt))).toLowerCase();
     }
 
     public static String sha1Hex(File file) throws IOException {
         FileInputStream fis = FileUtils.openInputStream(file);
         try {
-            return new String(Hex.encodeHex(DigestUtils.sha1(fis))).toLowerCase();
+            return new String(encodeHex(DigestUtils.sha1(fis))).toLowerCase();
         } finally {
             fis.close();
         }
