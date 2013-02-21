0885393c050be3ae989dbfd68b456b16aac8ae54
Mattias Linnap <mattias@linnap.com>
2013-02-21 15:21:39 +0000

    Improved settings for upload networks.

--- library/src/org/yousense/common/Time.java
+++ library/src/org/yousense/common/Time.java
@@ -9,6 +9,11 @@ import org.apache.commons.lang3.time.FastDateFormat;
 
 public class Time {
 
+    public static final long SECOND = 1000;
+    public static final long MINUTE = 60 * SECOND;
+    public static final long HOUR = 60 * MINUTE;
+    public static final long DAY = 24 * HOUR;
+
     public static TimeZone UTC = TimeZone.getTimeZone("UTC");
     public static Locale US = Locale.US;
     public static FastDateFormat NOSPACES_MILLISECONDS = FastDateFormat.getInstance("yyyyMMdd_HHmmss_SSS", UTC, US);
@@ -52,13 +57,27 @@ public class Time {
             sb.append("-");
             millis = -millis;
         }
-        long hours = millis / (3600*1000);
-        long minutes = (millis / (1000 * 60)) % 60;
+        long hours = millis / HOUR;
+        long minutes = (millis / MINUTE) % 60;
         sb.append(hours);
         sb.append("h ");
         sb.append(minutes);
         sb.append("min");
         return sb.toString();
+    }
+
+    /**
+     * Returns localised strings like "1 day ago" or "5 min ago".
+     */
+    public static String approximateTimeSinceFormat(long millis, String localisedNow, String localisedMin, String localisedHours, String localisedDays, String localisedAgo) {
+        if (millis < MINUTE)
+            return localisedNow;
+        else if (millis < HOUR)
+            return String.format("%d %s %s", millis / MINUTE, localisedMin, localisedAgo);
+        else if (millis < DAY)
+            return String.format("%d %s %s", millis / HOUR, localisedHours, localisedAgo);
+        else
+            return String.format("%d %s %s", millis / DAY, localisedDays, localisedAgo);
     }
 
     public static void sleepIgnoreInterrupt(long millis) {
